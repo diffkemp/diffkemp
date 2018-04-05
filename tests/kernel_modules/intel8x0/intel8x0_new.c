c90e2a0bd142b44a6efe1dd966de2e9a8bca4829
Viktor Malik <viktor.malik@gmail.com>
2018-04-05 13:31:02 +0200

    Port remaining tests to new testing interface and to LLVM 5.0

--- tests/kernel_modules/intel8x0/intel8x0_new.c
+++ tests/kernel_modules/intel8x0/intel8x0_new.c
@@ -40,9 +40,7 @@
 #include <sound/initval.h>
 /* for 440MX workaround */
 #include <asm/pgtable.h>
-#ifdef CONFIG_X86
-#include <asm/set_memory.h>
-#endif
+#include <asm/cacheflush.h>
 
 MODULE_AUTHOR("Jaroslav Kysela <perex@perex.cz>");
 MODULE_DESCRIPTION("Intel 82801AA,82901AB,i810,i820,i830,i840,i845,MX440; SiS 7012; Ali 5455");
