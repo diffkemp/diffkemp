3ae8260f125d46cd81a43c44ac222f3fae2ce52e
Tomáš Glozar <tglozar@gmail.com>
2018-08-21 10:57:30 +0200

    SimpLL: Assign global numbers to strings and APInts based on value instead of name.

--- diffkemp/simpll/DifferentialGlobalNumberState.cpp
+++ diffkemp/simpll/DifferentialGlobalNumberState.cpp
@@ -20,20 +20,70 @@
 uint64_t DifferentialGlobalNumberState::getNumber(GlobalValue *value) {
     auto number = GlobalNumbers.find(value);
     u_int64_t result;
+
     if (number == GlobalNumbers.end()) {
         // Get new number for the global value
         result = nextNumber;
-        GlobalNumbers.insert({value, nextNumber});
 
         // Try to find global value with the same name in the other module, if
         // it exists, assign it the same number
         auto otherModule = value->getParent() == First ? Second : First;
-        GlobalValue *otherValue = otherModule->getNamedValue(
-                value->getName());
-        if (otherValue)
-            GlobalNumbers.insert({otherValue, nextNumber});
+        auto GA = dyn_cast<GlobalVariable>(value);
 
-        nextNumber++;
+        // If the value is a GlobalVariable, has the attribute unnamed_addr and
+        // the initializer is either a ConstantDataSequential or a ConstantInt,
+        // use a map for comparison by value instead of name
+        if (GA && GA->hasGlobalUnnamedAddr() && GA->hasInitializer() &&
+            (dyn_cast<ConstantDataSequential>(GA->getInitializer()) ||
+             dyn_cast<ConstantInt>(GA->getInitializer()))) {
+            // Value is a constant that can be compared by value
+            Constant *I = GA->getInitializer();
+
+            if (ConstantDataSequential *CDS =
+                dyn_cast<ConstantDataSequential>(I)) {
+                // Value is a string - look it up in the string map
+                auto possibleResult = Strings.find(
+                    CDS->getAsString());
+
+                if(possibleResult != Strings.end()) {
+                    // If the string is in the map, set the result to it
+                    result = (*possibleResult).getValue();
+                } else {
+                    // If it isn't, assign it the next number and insert it to
+                    // both GlobalNumbers and the string map
+                    Strings.insert({CDS->getAsString(), nextNumber});
+                    GlobalNumbers.insert({value, nextNumber});
+
+                    result = nextNumber;
+                    nextNumber++;
+                }
+            } else if (ConstantInt *CI =
+                    dyn_cast<ConstantInt>(I)) {
+                auto possibleResult = Constants.find(CI->getValue());
+
+                if (possibleResult != Constants.end()) {
+                    // If the APInt is in the map, set the result to it
+                    result = (*possibleResult).second;
+                } else {
+                    // If it isn't, assign it the next number and insert it to
+                    // both GlobalNumbers and the APInt map
+                    Constants.insert({CI->getValue(), nextNumber});
+                    GlobalNumbers.insert({value, nextNumber});
+
+                    result = nextNumber;
+                    nextNumber++;
+                }
+            }
+        } else {
+            GlobalNumbers.insert({value, nextNumber});
+
+            GlobalValue *otherValue = otherModule->getNamedValue(
+                    value->getName());
+            if (otherValue)
+                GlobalNumbers.insert({otherValue, nextNumber});
+
+            nextNumber++;
+        }
     } else {
         // If number for the global value exists, return it
         result = number->second;
