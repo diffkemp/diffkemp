75b6ade202602568490350b0034d9cd284706c77
tglozar <tglozar@gmail.com>
2019-08-13 11:50:08 +0200

    SimpLL: Extend anonymous union numbering to structures.
    The original version of the pass didn't include this, because it
    is not without problems (e.g. when there is a change of a
    structure insinde another stucture, this wouldn't work), but it
    turned out to be useful.

--- diffkemp/simpll/passes/StructHashGeneratorPass.cpp
+++ diffkemp/simpll/passes/StructHashGeneratorPass.cpp
@@ -1,4 +1,4 @@
-//===-- UnionHashGeneratorPass.h - Renaming union types based on content --===//
+//=== StructHashGeneratorPass.cpp - Renaming struct types based on content ===//
 //
 //       SimpLL - Program simplifier for analysis of semantic difference      //
 //
@@ -7,16 +7,16 @@
 //===----------------------------------------------------------------------===//
 ///
 /// \file
-/// This file contains the declaration of the UnionHashGeneratorPass pass.
+/// This file contains the declaration of the StructHashGeneratorPass pass.
 ///
 //===----------------------------------------------------------------------===//
 
-#include "UnionHashGeneratorPass.h"
+#include "StructHashGeneratorPass.h"
 #include <llvm/ADT/Hashing.h>
 #include <llvm/IR/TypeFinder.h>
 #include <llvm/Support/raw_ostream.h>
 
-PreservedAnalyses UnionHashGeneratorPass::run (
+PreservedAnalyses StructHashGeneratorPass::run (
         Module &Mod,
         llvm::AnalysisManager<llvm::Module> &Main) {
     TypeFinder Types;
@@ -24,15 +24,21 @@ PreservedAnalyses UnionHashGeneratorPass::run (
 
     for (auto *Ty : Types) {
         if (auto STy = dyn_cast<StructType>(Ty)) {
-            if (!STy->getStructName().startswith("union.anon"))
+            if (!STy->getStructName().startswith("union.anon") &&
+                !STy->getStructName().startswith("struct.anon"))
                 continue;
             std::string TypeName = STy->getStructName().str();
             std::string TypeDump; llvm::raw_string_ostream DumpStrm(TypeDump);
             DumpStrm << *STy; TypeDump = DumpStrm.str();
 
             // Extract the type declaration without type name
-            std::string TypeDecl = TypeDump.substr(TypeDump.find("{"));
-            std::string NewTypeName = "union.anon." +
+            int pos = TypeDump.find("{");
+            if (pos == std::string::npos)
+                continue;
+            std::string TypeDecl = TypeDump.substr(pos);
+            std::string NewTypeName =
+                    STy->getStructName().startswith("union.anon") ?
+                            "union.anon." : "struct.anon:" +
                     std::to_string(hash_value(TypeDecl));
 
             // Rename the type
