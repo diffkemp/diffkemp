183c03ad4921ca9faf6cd9ab10394c89eb5bff99
tglozar <tglozar@gmail.com>
2019-09-11 07:24:10 +0000

    SimpLL: Improve RemoveUnusedReturnValuesPass.
    Instead of checking whether all uses of the value are call or invoke
    instuctions of which the return values are not used and using that
    as a condition for continuing to process the function, the pass
    collects such uses into a vector and splits the function into two -
    one without a return value for the collected uses (where it is
    replaced the same way as in the previous version of the pass), and
    one for all other cases (indirect calls, uses inside constant
    expressions and so on; the replacing is done using
    replaceAllUsesWith) that is a clone of the original function.

--- diffkemp/simpll/passes/RemoveUnusedReturnValuesPass.cpp
+++ diffkemp/simpll/passes/RemoveUnusedReturnValuesPass.cpp
@@ -17,6 +17,7 @@
 #include <Config.h>
 #include <llvm/IR/Instructions.h>
 #include <llvm/IR/Intrinsics.h>
+#include <llvm/Transforms/Utils/Cloning.h>
 
 PreservedAnalyses RemoveUnusedReturnValuesPass::run(
         Module &Mod,
@@ -43,211 +44,221 @@ PreservedAnalyses RemoveUnusedReturnValuesPass::run(
 
     auto &CalledFuns = mam.getResult<CalledFunctionsAnalysis>(Mod, Main);
 
-    // Old functions ought to be deleted after iteration
-    std::vector<Function *> functionsToDelete;
+    // Initial list of functions to iterate over.
+    std::vector<Function *> functionsToIterateOver;
+    for (Function &Fun : Mod)
+        functionsToIterateOver.push_back(&Fun);
 
-    for (Function &Fun : Mod) {
-        if (Fun.getIntrinsicID() != llvm::Intrinsic::not_intrinsic)
+    for (Function *Fun : functionsToIterateOver) {
+        if (Fun->getIntrinsicID() != llvm::Intrinsic::not_intrinsic)
             continue;
 
-        if (Fun.getReturnType()->isVoidTy())
+        if (Fun->getReturnType()->isVoidTy())
             continue;
 
-        if (!ModOther->getFunction(Fun.getName()))
+        if (!ModOther->getFunction(Fun->getName()))
             continue;
 
-        if (!ModOther->getFunction(Fun.getName())->getReturnType()->isVoidTy())
+        if (!ModOther->getFunction(Fun->getName())->getReturnType()->isVoidTy())
             continue;
 
-        if (CalledFuns.find(&Fun) == CalledFuns.end())
+        if (CalledFuns.find(Fun) == CalledFuns.end())
             continue;
 
-        bool can_replace = true;
-        DEBUG_WITH_TYPE(DEBUG_SIMPLL,
-                        dbgs() << "Changing function: " << Fun.getName()
-                               << " to void\n");
-        for (Use &U : Fun.uses()) {
-            // Figure out whether the return value is used after each call
+        std::vector<Instruction *> toReplace;
 
-            if (auto CI = dyn_cast<CallInst>(U.getUser())) {
-                if (CI->getCalledFunction() != &Fun)
-                    // Different function is called, Fun is an argument
-                    can_replace = false;
-                DEBUG_WITH_TYPE(DEBUG_SIMPLL, {
-                    CI->print(dbgs());
-                    for (Use &UU : CI->uses()) {
-                        dbgs() << "  ";
-                        UU.getUser()->print(dbgs());
-                    }
-                });
-                if (!CI->use_empty())
-                    // The return value is actually used
-                    can_replace = false;
-            } else if (auto II = dyn_cast<InvokeInst>(U.getUser())) {
-                if (II->getCalledFunction() != &Fun)
-                    // Different function is called, Fun is an argument
-                    can_replace = false;
-                DEBUG_WITH_TYPE(DEBUG_SIMPLL, {
-                    II->print(dbgs());
-                    for (Use &UU : II->uses()) {
-                        dbgs() << "  ";
-                        UU.getUser()->print(dbgs());
-                    }
-                });
-                if (!II->use_empty())
-                    // The return value is actually used
-                    can_replace = false;
-            } else
-                // The function is used somewhere as an argument, therefore
-                // it ought not to be replaced
-                can_replace = false;
+        for (Use &U : Fun->uses()) {
+            // Figure out whether the return value is used after each call.
+            if (U.getUser()->use_empty() &&
+                ((isa<CallInst>(U.getUser()) && (dyn_cast<CallInst>(
+                    U.getUser())->getCalledFunction()) == Fun) ||
+                (isa<InvokeInst>(U.getUser()) && (dyn_cast<InvokeInst>(
+                    U.getUser())->getCalledFunction()) == Fun))) {
+                // The instruction can be replaced.
+                toReplace.push_back(dyn_cast<Instruction>(U.getUser()));
+            }
         }
 
-        if (can_replace) {
-            // Create the header of the new function
-            std::vector<Type *> FAT_New(Fun.getFunctionType()->param_begin(),
-                                        Fun.getFunctionType()->param_end());
-            FunctionType *FT_New = FunctionType::get(
-                    Type::getVoidTy(Fun.getContext()),
-                    FAT_New, Fun.isVarArg());
-            Function *Fun_New = Function::Create(FT_New,
-                                                 Fun.getLinkage(),
-                                                 Fun.getName(),
-                                                 Fun.getParent());
+        if (toReplace.empty())
+            // Nothing to replace.
+            continue;
 
-            // Copy the attributes from the old function and delete the ones
-            // related to the return value
-            Fun_New->copyAttributesFrom(&Fun);
-            for (Attribute::AttrKind AK : badAttributes) {
-                Fun_New->removeAttribute(AttributeList::ReturnIndex, AK);
-                Fun_New->removeAttribute(AttributeList::FunctionIndex, AK);
-            }
-            Fun_New->setAttributes(
-                cleanAttributeList(Fun_New->getAttributes()));
+        DEBUG_WITH_TYPE(DEBUG_SIMPLL,
+                        dbgs() << getDebugIndent(' ')
+                               << "Creating void-returning variant of "
+                               << Fun->getName() << "\n");
 
-            // Set the right function name and subprogram
-            Fun_New->takeName(&Fun);
-            Fun_New->setSubprogram(Fun.getSubprogram());
-
-            // Set the names of all arguments of the new function
-            for (Function::arg_iterator AI = Fun.arg_begin(),
-                         AE = Fun.arg_end(), NAI = Fun_New->arg_begin();
+        // Create a clone of the function.
+        // Note: this is needed because the arguments of the original function
+        // are going to be replaced with the arguments of the replacement
+        // function in the whole module including the original function, which
+        // ends up unusable, and therefore is deleted and replaced with the
+        // clone. (Another solution would be to replace the uses manually, but
+        // this is an easier solution.)
+        ValueToValueMapTy Map;
+        std::string OriginalName = Fun->getName();
+        Fun->setName("");
+        Function *Fun_Clone;
+        if (!Fun->isDeclaration())
+            Fun_Clone = CloneFunction(Fun, Map);
+        else {
+            // CloneFunction support only functions with a full body (possibly
+            // because of a bug). Handle declarations separately.
+            Fun_Clone = Function::Create(Fun->getFunctionType(),
+                                         Fun->getLinkage(),
+                                         OriginalName,
+                                         Fun->getParent());
+            Fun_Clone->copyAttributesFrom(Fun);
+            Fun_Clone->setSubprogram(Fun->getSubprogram());
+            for (Function::arg_iterator AI = Fun->arg_begin(),
+                    AE = Fun->arg_end(), NAI = Fun_Clone->arg_begin();
                  AI != AE;
                  ++AI, ++NAI) {
                 NAI->takeName(AI);
             }
-
-            // Copy the function body (currently not used, because function with
-            // a body are ignored)
-            Fun_New->getBasicBlockList().splice(Fun_New->begin(),
-                                                Fun.getBasicBlockList());
-
-            // Replace return instructions on ends of basic blocks with ret void
-            // (currently not used because function with a body are ignored)
-            for (BasicBlock &B : *Fun_New)
-                if (dyn_cast<ReturnInst>(B.getTerminator())) {
-                    B.getInstList().pop_back();
-                    ReturnInst *Term_New = ReturnInst::Create(
-                            B.getContext());
-                    B.getInstList().push_back(Term_New);
-
-                    // Simplify the function to remove any code that became dead
-                    simplifyFunction(Fun_New);
-                }
-
-            // Replace all uses of the old arguments
-            for (Function::arg_iterator I = Fun.arg_begin(),
-                         E = Fun.arg_end(), NI = Fun_New->arg_begin();
-                 I != E;
-                 ++I, ++NI) {
-                I->replaceAllUsesWith(NI);
-            }
-
-            // For call or invoke instructions a new instruction has to be
-            // created and the old one replaced
-            for (Use &U : Fun.uses()) {
-                if (CallInst *CI = dyn_cast<CallInst>(U.getUser())) {
-                    // First copy all arguments to an array
-                    // and create the new instruction
-                    std::vector<Value *> Args;
-
-                    for (Value *A : CI->arg_operands()) {
-                        Args.push_back(A);
-                    }
-
-                    ArrayRef<Value *> Args_AR(Args);
-
-                    // Insert the new instruction next to the old one
-                    CallInst *CI_New = CallInst::Create(Fun_New, Args_AR, "",
-                                                        CI);
-
-                    // Copy additional properties
-                    CI_New->setAttributes(CI->getAttributes());
-                    for (Attribute::AttrKind AK : badAttributes) {
-                        // Remove incompatibile attributes
-                        CI_New->removeAttribute(
-                                AttributeList::ReturnIndex, AK);
-                        CI_New->removeAttribute(
-                                AttributeList::FunctionIndex, AK);
-                    }
-                    CI_New->setAttributes(
-                        cleanAttributeList(CI_New->getAttributes()));
-                    CI_New->setDebugLoc(CI->getDebugLoc());
-                    CI_New->setCallingConv(CI->getCallingConv());
-                    if (CI->isTailCall())
-                        CI_New->setTailCall();
-                    DEBUG_WITH_TYPE(DEBUG_SIMPLL,
-                                    dbgs() << "Replacing :" << *CI << " with "
-                                           << *CI_New << "\n");
-                    // Erase the old instruction
-                    CI->eraseFromParent();
-                } else if (InvokeInst *II = dyn_cast<InvokeInst>(U.getUser())) {
-                    // First copy all arguments to an array and create
-                    // the new instruction
-                    std::vector<Value *> Args;
-
-                    for (Value *A : II->arg_operands()) {
-                        Args.push_back(A);
-                    }
-
-                    ArrayRef<Value *> Args_AR(Args);
-
-                    // Insert the new instruction next to the old one
-                    InvokeInst *II_New = InvokeInst::Create(
-                            Fun_New,
-                            II->getNormalDest(),
-                            II->getUnwindDest(),
-                            Args_AR, "", II);
-
-                    // Copy additional properties
-                    II_New->setAttributes(II->getAttributes());
-                    for (Attribute::AttrKind AK : badAttributes) {
-                        // Remove incompatibile attributes
-                        II_New->removeAttribute(
-                                AttributeList::ReturnIndex, AK);
-                        II_New->removeAttribute(
-                                AttributeList::FunctionIndex, AK);
-                    }
-                    II_New->setAttributes(
-                        cleanAttributeList(II_New->getAttributes()));
-                    II_New->setDebugLoc(II->getDebugLoc());
-                    II_New->setCallingConv(II->getCallingConv());
-                    DEBUG_WITH_TYPE(DEBUG_SIMPLL,
-                                    dbgs() << "Replacing :" << *II << " with "
-                                           << *II_New << "\n");
-                    // Erase the old instruction
-                    II->eraseFromParent();
-                }
-            }
-            DEBUG_WITH_TYPE(DEBUG_SIMPLL, Fun_New->print(dbgs()));
-            // Delete function after iteration
-            functionsToDelete.push_back(&Fun);
         }
-    }
+        Fun_Clone->setName(OriginalName);
 
-    // Delete replaced functions
-    for (Function *F : functionsToDelete)
-        F->removeFromParent();
+        // Create the header of the new function.
+        std::vector<Type *> FAT_New(Fun->getFunctionType()->param_begin(),
+                                    Fun->getFunctionType()->param_end());
+        FunctionType *FT_New = FunctionType::get(
+                Type::getVoidTy(Fun->getContext()),
+                FAT_New, Fun->isVarArg());
+        Function *Fun_New = Function::Create(FT_New,
+                                             Fun->getLinkage(),
+                                             Fun->getName(),
+                                             Fun->getParent());
+
+        // Copy the attributes from the old function and delete the ones
+        // related to the return value
+        Fun_New->copyAttributesFrom(Fun);
+        for (Attribute::AttrKind AK : badAttributes) {
+            Fun_New->removeAttribute(AttributeList::ReturnIndex, AK);
+            Fun_New->removeAttribute(AttributeList::FunctionIndex, AK);
+        }
+        Fun_New->setAttributes(
+            cleanAttributeList(Fun_New->getAttributes()));
+
+        // Set the right function name and subprogram
+        Fun_New->takeName(Fun_Clone);
+        Fun_New->setSubprogram(Fun->getSubprogram());
+
+        // Set the names of all arguments of the new function
+        for (Function::arg_iterator AI = Fun->arg_begin(),
+                     AE = Fun->arg_end(), NAI = Fun_New->arg_begin();
+             AI != AE;
+             ++AI, ++NAI) {
+            NAI->takeName(AI);
+        }
+
+        // Copy the function body.
+        Fun_New->getBasicBlockList().splice(Fun_New->begin(),
+                                            Fun->getBasicBlockList());
+
+        // Replace return instructions on ends of basic blocks with ret void
+        for (BasicBlock &B : *Fun_New)
+            if (dyn_cast<ReturnInst>(B.getTerminator())) {
+                B.getInstList().pop_back();
+                ReturnInst *Term_New = ReturnInst::Create(
+                        B.getContext());
+                B.getInstList().push_back(Term_New);
+
+                // Simplify the function to remove any code that became dead
+                simplifyFunction(Fun_New);
+            }
+
+        // Replace all uses of the old arguments
+        for (Function::arg_iterator I = Fun->arg_begin(),
+                     E = Fun->arg_end(), NI = Fun_New->arg_begin();
+             I != E;
+             ++I, ++NI) {
+            I->replaceAllUsesWith(NI);
+        }
+
+        // For call or invoke instructions where the return value is not used
+        // a new instruction has to be created and the old one replaced.
+        for (Instruction *I : toReplace) {
+            if (CallInst *CI = dyn_cast<CallInst>(I)) {
+                // First copy all arguments to an array
+                // and create the new instruction
+                std::vector<Value *> Args;
+
+                for (Value *A : CI->arg_operands()) {
+                    Args.push_back(A);
+                }
+
+                ArrayRef<Value *> Args_AR(Args);
+
+                // Insert the new instruction next to the old one
+                CallInst *CI_New = CallInst::Create(Fun_New, Args_AR, "",
+                                                    CI);
+
+                // Copy additional properties
+                CI_New->setAttributes(CI->getAttributes());
+                for (Attribute::AttrKind AK : badAttributes) {
+                    // Remove incompatibile attributes
+                    CI_New->removeAttribute(
+                            AttributeList::ReturnIndex, AK);
+                    CI_New->removeAttribute(
+                            AttributeList::FunctionIndex, AK);
+                }
+                CI_New->setAttributes(
+                    cleanAttributeList(CI_New->getAttributes()));
+                CI_New->setDebugLoc(CI->getDebugLoc());
+                CI_New->setCallingConv(CI->getCallingConv());
+                if (CI->isTailCall())
+                    CI_New->setTailCall();
+                DEBUG_WITH_TYPE(DEBUG_SIMPLL,
+                                dbgs() << "Replacing :" << *CI << " with "
+                                       << *CI_New << "\n");
+                // Erase the old instruction
+                CI->eraseFromParent();
+            } else if (InvokeInst *II = dyn_cast<InvokeInst>(I)) {
+                // First copy all arguments to an array and create
+                // the new instruction
+                std::vector<Value *> Args;
+
+                for (Value *A : II->arg_operands()) {
+                    Args.push_back(A);
+                }
+
+                ArrayRef<Value *> Args_AR(Args);
+
+                // Insert the new instruction next to the old one
+                InvokeInst *II_New = InvokeInst::Create(
+                        Fun_New,
+                        II->getNormalDest(),
+                        II->getUnwindDest(),
+                        Args_AR, "", II);
+
+                // Copy additional properties
+                II_New->setAttributes(II->getAttributes());
+                for (Attribute::AttrKind AK : badAttributes) {
+                    // Remove incompatibile attributes
+                    II_New->removeAttribute(
+                            AttributeList::ReturnIndex, AK);
+                    II_New->removeAttribute(
+                            AttributeList::FunctionIndex, AK);
+                }
+                II_New->setAttributes(
+                    cleanAttributeList(II_New->getAttributes()));
+                II_New->setDebugLoc(II->getDebugLoc());
+                II_New->setCallingConv(II->getCallingConv());
+                DEBUG_WITH_TYPE(DEBUG_SIMPLL,
+                                dbgs() << "Replacing :" << *II << " with "
+                                       << *II_New << "\n");
+                // Erase the old instruction
+                II->eraseFromParent();
+            }
+        }
+
+        // Replace all other uses of the function with its clone.
+        Fun->replaceAllUsesWith(Fun_Clone);
+
+        // Delete function.
+        Fun->eraseFromParent();
+    }
 
     return PreservedAnalyses();
 }
