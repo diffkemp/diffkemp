3fc220057d9ecf414cddac0af8f3e5ba135042bf
tglozar <tglozar@gmail.com>
2019-09-13 12:03:44 +0200

    SimpLL: Preprocess functions in global variables.

--- diffkemp/simpll/passes/CalledFunctionsAnalysis.cpp
+++ diffkemp/simpll/passes/CalledFunctionsAnalysis.cpp
@@ -15,6 +15,7 @@
 #include "CalledFunctionsAnalysis.h"
 #include "Utils.h"
 #include <llvm/IR/Instructions.h>
+#include <llvm/IR/User.h>
 
 AnalysisKey CalledFunctionsAnalysis::Key;
 
@@ -48,7 +49,28 @@ void CalledFunctionsAnalysis::collectCalled(const Function *Fun,
             for (auto &Op : Inst.operands()) {
                 if (auto *CalledFun = getCalledFunction(Op))
                     collectCalled(CalledFun, Called);
+                if (isa<GlobalVariable>(Op)) {
+                    auto GV = dyn_cast<GlobalVariable>(&Op);
+                    if (GV->hasInitializer() && GV->isConstant()) {
+                        // The initializer is constant - see whether it contains
+                        // a function (or a user type constant that contains
+                        // a function).
+                        processValue(GV->getInitializer(), Called);
+                    }
+                }
             }
+        }
+    }
+}
+
+/// Looks for functions in a value (either a function itself, or a composite
+/// type constant).
+void CalledFunctionsAnalysis::processValue(const Value *Val, Result &Called) {
+    if (auto Fun = dyn_cast<Function>(Val))
+        collectCalled(Fun, Called);
+    else if (auto U = dyn_cast<User>(Val)) {
+        for (auto &Value : U->operands()) {
+            processValue(Value.get(), Called);
         }
     }
 }
