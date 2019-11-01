c15c54a33ce2c8ec5efa07856153e587dceb3eab
Viktor Malik <viktor.malik@gmail.com>
2019-11-01 13:42:49 +0100

    SimpLL: fix bug in CalledFunctionsAnalysis
    Before this fix, if a function was stored inside a global constant that
    was used in a bitcast operator, the function was not added to called
    functions (due to missing support of the bitcast operator).

--- diffkemp/simpll/passes/CalledFunctionsAnalysis.cpp
+++ diffkemp/simpll/passes/CalledFunctionsAnalysis.cpp
@@ -15,6 +15,7 @@
 #include "CalledFunctionsAnalysis.h"
 #include "Utils.h"
 #include <llvm/IR/Instructions.h>
+#include <llvm/IR/Operator.h>
 #include <llvm/IR/User.h>
 
 AnalysisKey CalledFunctionsAnalysis::Key;
@@ -45,17 +46,7 @@ void CalledFunctionsAnalysis::collectCalled(const Function *Fun,
                 }
             }
             for (auto &Op : Inst.operands()) {
-                if (auto *CalledFun = getCalledFunction(Op))
-                    collectCalled(CalledFun, Called);
-                if (isa<GlobalVariable>(Op)) {
-                    auto GV = dyn_cast<GlobalVariable>(&Op);
-                    if (GV->hasInitializer() && GV->isConstant()) {
-                        // The initializer is constant - see whether it contains
-                        // a function (or a user type constant that contains
-                        // a function).
-                        processValue(GV->getInitializer(), Called);
-                    }
-                }
+                processValue(Op, Called);
             }
         }
     }
@@ -64,11 +55,21 @@ void CalledFunctionsAnalysis::collectCalled(const Function *Fun,
 /// Looks for functions in a value (either a function itself, or a composite
 /// type constant).
 void CalledFunctionsAnalysis::processValue(const Value *Val, Result &Called) {
-    if (auto Fun = dyn_cast<Function>(Val))
+    if (auto Fun = getCalledFunction(Val))
         collectCalled(Fun, Called);
-    else if (auto U = dyn_cast<User>(Val)) {
-        for (auto &Value : U->operands()) {
-            processValue(Value.get(), Called);
-        }
+    else if (auto GV = dyn_cast<GlobalVariable>(Val)) {
+        if (GV->hasInitializer() && GV->isConstant())
+            // The initializer is constant - see whether it contains
+            // a function (or a user type constant that contains
+            // a function).
+            if (auto U = dyn_cast<User>(GV->getInitializer())) {
+                for (auto &UserOp : U->operands()) {
+                    processValue(UserOp.get(), Called);
+                }
+            } else {
+                processValue(GV->getInitializer(), Called);
+            }
+    } else if (auto BitCast = dyn_cast<BitCastOperator>(Val)) {
+        processValue(BitCast->getOperand(0), Called);
     }
 }
