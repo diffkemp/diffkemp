0b84ff96158850f42f510d70fc0da8104a1633a4
psilling <petr.silling@centrum.cz>
2020-02-07 14:05:47 +0100

    Change FunctionList to Snapshot
    In the new Snapshot include a list of functions just like in
    FunctionList. However, represent the whole snapshot directory with
    kernel sources, not just the function list. Also update the YAML file
    structure and rename it to 'snapshot.yaml'.

---
+++ diffkemp/snapshot.py
@@ -0,0 +1,234 @@
+"""
+Kernel snapshot representation.
+Contains mappings of original kernel source, snapshot kernel source and
+holds the function list.
+"""
+from diffkemp.llvm_ir.kernel_module import LlvmKernelModule
+from diffkemp.llvm_ir.kernel_source import KernelSource
+import os
+import pkg_resources
+import shutil
+import yaml
+
+
+class Snapshot:
+    """
+    Kernel snapshot.
+    Provides methods for snapshot management. Also includes descriptions of
+    functions and sysctl options.
+    """
+
+    class FunctionDesc:
+        """
+        Function description.
+        Contains LLVM module of the function definition, a global variable,
+        and a tag.
+        For sysctl option, glob_var is the data variable that the option sets.
+        """
+
+        def __init__(self, mod, glob_var, tag):
+            self.mod = mod
+            self.glob_var = glob_var
+            self.tag = tag
+
+    class FunctionGroup:
+        """
+        Group of functions. Used to group functions belonging to a single
+        sysctl options.
+        """
+
+        def __init__(self):
+            self.functions = dict()
+
+    def __init__(self, kernel_source=None, snapshot_source=None,
+                 fun_kind=None):
+        self.kernel_source = kernel_source
+        self.snapshot_source = snapshot_source
+        self.fun_kind = fun_kind
+        self.fun_groups = dict()
+
+    @classmethod
+    def create_from_source(cls, kernel_dir, output_dir, fun_kind=None,
+                           setup_dir=True):
+        """
+        Create a snapshot from a kernel source directory and prepare it for
+        snapshot directory generation.
+        :param kernel_dir: Source kernel directory.
+        :param output_dir: Snapshot output directory.
+        :param fun_kind: Snapshot function kind.
+        :param setup_dir: Whether to recreate the output directory.
+        :return: Desired instance of Snapshot.
+        """
+        output_path = os.path.abspath(output_dir)
+
+        # Cleanup or create the output directory of the new snapshot.
+        if setup_dir:
+            if os.path.isdir(output_path):
+                shutil.rmtree(output_path)
+            os.mkdir(output_path)
+
+        # Prepare source representations for the new snapshot
+        kernel_source = KernelSource(kernel_dir, True)
+        snapshot_source = KernelSource(output_path)
+
+        kernel_snapshot = cls(kernel_source, snapshot_source, fun_kind)
+
+        # Add a new None group to the snapshot if the function kind is None.
+        if fun_kind is None:
+            kernel_snapshot._add_none_group()
+
+        return kernel_snapshot
+
+    @classmethod
+    def load_from_dir(cls, snapshot_dir, config_file="snapshot.yaml"):
+        """
+        Loads a snapshot from its directory.
+        :param snapshot_dir: Target snapshot directory.
+        :param config_file: Name of the snapshot configuration file.
+        :return: Desired instance of Snapshot.
+        """
+        snapshot_source = KernelSource(snapshot_dir)
+        loaded_snapshot = cls(None, snapshot_source)
+
+        with open(os.path.join(snapshot_dir, config_file), "r") as \
+                snapshot_yaml:
+            loaded_snapshot._from_yaml(snapshot_yaml.read())
+
+        return loaded_snapshot
+
+    def finalize(self):
+        """Finalize both kernel sources."""
+        if self.kernel_source is not None:
+            self.kernel_source.finalize()
+        if self.snapshot_source is not None:
+            self.snapshot_source.finalize()
+
+    def generate_snapshot_dir(self):
+        """Generate the corresponding snapshot directory."""
+        # Copy LLVM files to the snapshot.
+        self.kernel_source.copy_source_files(self.modules(),
+                                             self.snapshot_source.kernel_dir)
+        self.snapshot_source.build_cscope_database()
+
+        # Create the YAML snapshot representation inside the output directory.
+        with open(os.path.join(self.snapshot_source.kernel_dir,
+                               "snapshot.yaml"), "w") as snapshot_yaml:
+            snapshot_yaml.write(self.to_yaml())
+
+    def add_fun(self, name, llvm_mod, glob_var=None, tag=None, group=None):
+        """
+        Add function to the function list.
+        :param name: Name of the function.
+        :param llvm_mod: LLVM module with the function definition.
+        :param tag: Function tag.
+        :param group: Group to put the function to.
+        """
+        self.fun_groups[group].functions[name] = self.FunctionDesc(llvm_mod,
+                                                                   glob_var,
+                                                                   tag)
+
+    def _add_none_group(self):
+        """
+        Add new group with None name which is used when only a single group
+        is used.
+        """
+        self.fun_groups[None] = self.FunctionGroup()
+
+    def add_fun_group(self, name):
+        """
+        Add new function group with the given name.
+        :param name: Name of the group.
+        """
+        self.fun_groups[name] = self.FunctionGroup()
+
+    def modules(self):
+        """
+        Get the set of all LLVM modules of all functions in the function list.
+        """
+        return set([fun.mod for group in self.fun_groups.values()
+                    for fun in group.functions.values()
+                    if fun.mod is not None])
+
+    def get_by_name(self, name, group=None):
+        """
+        Get module for the function with the given name in the given group.
+        :param name: Name of the function.
+        :param group: Group of the function.
+        """
+        return self.fun_groups[group].functions[name] \
+            if group in self.fun_groups and name in self.fun_groups[
+            group].functions \
+            else None
+
+    def filter(self, functions, group=None):
+        """
+        Filter only chosen functions in the given group.
+        :param name: List of function names to filter.
+        :param group: Group to filter.
+        """
+        self.fun_groups[group].functions = {f: self.fun_groups[
+            group].functions[f] for f in functions}
+
+    def _from_yaml(self, yaml_file):
+        """
+        Load the snaphot from its YAML representation. Paths are assumed to be
+        relative to the root directory.
+        :param yaml_file: Contents of the YAML file.
+        """
+        yaml_file = yaml.safe_load(yaml_file)
+        yaml_dict = yaml_file[0]
+
+        if os.path.isdir(yaml_dict["source_kernel_dir"]):
+            self.kernel_source = KernelSource(yaml_dict["source_kernel_dir"],
+                                              True)
+
+        for g in yaml_dict["list"]:
+            if "sysctl" in g:
+                self.kind = "sysctl"
+                group = g["sysctl"]
+                functions = g["functions"]
+            else:
+                group = None
+                functions = yaml_dict["list"]
+            self.fun_groups[group] = self.FunctionGroup()
+            for f in functions:
+                self.add_fun(f["name"],
+                             LlvmKernelModule(
+                                 os.path.join(os.path.relpath(
+                                     self.snapshot_source.kernel_dir),
+                                     f["llvm"]))
+                             if f["llvm"] else None,
+                             f["glob_var"],
+                             f["tag"],
+                             group)
+
+    def to_yaml(self):
+        """
+        Dump the snapshot as a YAML string. Paths to files are given relative
+        to the source snapshot directory.
+        :return: YAML string.
+        """
+        # Create the function group list.
+        fun_yaml_dict = [{
+            self.fun_kind: group_name,
+            "functions": [{
+                "name": fun_name,
+                "llvm": os.path.relpath(fun_desc.mod.llvm,
+                                        self.snapshot_source.kernel_dir)
+                if fun_desc.mod else None,
+                "glob_var": fun_desc.glob_var,
+                "tag": fun_desc.tag
+            } for fun_name, fun_desc in g.functions.items()]
+        } for group_name, g in self.fun_groups.items()
+        ]
+
+        # Create the top level YAML structure.
+        yaml_dict = [{
+            "diffkemp_version": pkg_resources.require("diffkemp")[0].version,
+            "kind": "function_list" if self.fun_kind is None else
+            "systcl_group_list",
+            "list": fun_yaml_dict[0]["functions"] if None in self.fun_groups
+            else fun_yaml_dict,
+            "source_kernel_dir": self.kernel_source.kernel_dir
+        }]
+        return yaml.dump(yaml_dict)
---
+++ tests/unit_tests/snapshot_test.py
@@ -0,0 +1,371 @@
+"""Unit tests for the Snapshot class."""
+
+from diffkemp.snapshot import Snapshot
+from diffkemp.llvm_ir.kernel_module import LlvmKernelModule
+from diffkemp.llvm_ir.kernel_source import KernelSource
+from tempfile import NamedTemporaryFile
+from tempfile import TemporaryDirectory
+import os
+import yaml
+
+
+def test_create_snapshot_from_source():
+    """Create a new kernel directory snapshot."""
+    kernel_dir = "kernel/linux-3.10.0-957.el7"
+    output_dir = "snapshots/linux-3.10.0-957.el7"
+    snap = Snapshot.create_from_source(kernel_dir, output_dir,
+                                       None, False)
+
+    assert snap.kernel_source is not None
+    assert kernel_dir in snap.kernel_source.kernel_dir
+    assert snap.snapshot_source is not None
+    assert output_dir in snap.snapshot_source.kernel_dir
+    assert snap.fun_kind is None
+    assert len(snap.fun_groups) == 1
+    assert len(snap.fun_groups[None].functions) == 0
+
+    snap = Snapshot.create_from_source(kernel_dir, output_dir,
+                                       "sysctl", False)
+
+    assert snap.fun_kind == "sysctl"
+    assert len(snap.fun_groups) == 0
+
+
+def test_load_snapshot_from_dir_functions():
+    """
+    Create a temporary snapshot directory and try to parse it. Use a YAML
+    configuration file that contains only a list of functions. Expect that
+    the function list inside the parsed snapshot contains a single "None"
+    group which contains the list of loaded functions. All parsed LLVM paths
+    should contain the list root dir.
+    """
+    with TemporaryDirectory(prefix="test_snapshots_") as snap_dir, \
+            NamedTemporaryFile(mode="w+t", prefix="snapshot_", suffix=".yaml",
+                               dir=snap_dir) as config_file:
+        # Populate the temporary snapshot configuration file.
+        config_file.writelines("""
+        - diffkemp_version: '0.1'
+          kind: function_list
+          list:
+          - glob_var: null
+            llvm: net/core/skbuff.ll
+            name: ___pskb_trim
+            tag: null
+          - glob_var: null
+            llvm: mm/page_alloc.ll
+            name: __alloc_pages_nodemask
+            tag: null
+          source_kernel_dir: /diffkemp/kernel/linux-3.10.0-957.el7
+        """)
+
+        # Load the temporary snapshot configuration file.
+        config_file.seek(0)
+        config_filename = os.path.basename(config_file.name)
+        snap = Snapshot.load_from_dir(snap_dir, config_filename)
+
+        assert isinstance(snap.snapshot_source, KernelSource)
+        assert snap.snapshot_source.kernel_dir == snap_dir
+        assert len(snap.fun_groups) == 1
+        assert None in snap.fun_groups
+        assert len(snap.fun_groups[None].functions) == 2
+        assert set(snap.fun_groups[None].functions.keys()) == \
+            {"___pskb_trim",
+             "__alloc_pages_nodemask"}
+
+        for name, f in snap.fun_groups[None].functions.items():
+            assert f.glob_var is None
+            assert f.tag is None
+            if name == "___pskb_trim":
+                assert os.path.abspath(f.mod.llvm) == snap_dir + \
+                       "/net/core/skbuff.ll"
+            elif name == "__alloc_pages_nodemask":
+                assert os.path.abspath(f.mod.llvm) == snap_dir + \
+                       "/mm/page_alloc.ll"
+
+
+def test_load_snapshot_from_dir_sysctls():
+    """
+    Create a temporary snapshot directory and try to parse it. Use a YAML
+    configuration file that contains a list of sysctl groups, each group
+    containing a single function. All parsed LLVM paths should contain the list
+    root dir.
+    """
+    with TemporaryDirectory(prefix="test_snapshots_sysctl_") as snap_dir, \
+            NamedTemporaryFile(mode="w+t", prefix="snapshot_", suffix=".yaml",
+                               dir=snap_dir) as config_file:
+        # Populate the temporary sysctl snapshot configuration file.
+        config_file.writelines("""
+        - diffkemp_version: '0.1'
+          kind: function_list
+          list:
+          - functions:
+            - glob_var: null
+              llvm: kernel/sched/fair.ll
+              name: sched_proc_update_handler
+              tag: proc handler
+            sysctl: kernel.sched_latency_ns
+          - functions:
+            - glob_var: null
+              llvm: kernel/sysctl.ll
+              name: proc_dointvec_minmax
+              tag: proc handler
+            sysctl: kernel.timer_migration
+          source_kernel_dir: /diffkemp/kernel/linux-3.10.0-957.el7
+        """)
+
+        # Load the temporary sysctl snapshot configuration file.
+        config_file.seek(0)
+        config_filename = os.path.basename(config_file.name)
+        snap = Snapshot.load_from_dir(snap_dir, config_filename)
+
+        assert len(snap.fun_groups) == 2
+        assert set(snap.fun_groups.keys()) == {"kernel.sched_latency_ns",
+                                               "kernel.timer_migration"}
+
+        for name, g in snap.fun_groups.items():
+            f = None
+            assert len(g.functions) == 1
+            if name == "kernel.sched_latency_ns":
+                assert g.functions.keys() == {"sched_proc_update_handler"}
+                f = g.functions["sched_proc_update_handler"]
+                assert os.path.abspath(f.mod.llvm) == snap_dir + \
+                    "/kernel/sched/fair.ll"
+            elif name == "kernel.timer_migration":
+                assert g.functions.keys() == {"proc_dointvec_minmax"}
+                f = g.functions["proc_dointvec_minmax"]
+                assert os.path.abspath(f.mod.llvm) == snap_dir + \
+                    "/kernel/sysctl.ll"
+            assert f.tag == "proc handler"
+            assert f.glob_var is None
+
+
+def test_add_sysctl_fun_group():
+    """Create a snapshot and check the creation of a sysctl function group."""
+
+    kernel_dir = "kernel/linux-3.10.0-957.el7"
+    output_dir = "snapshots/linux-3.10.0-957.el7"
+    snap = Snapshot.create_from_source(kernel_dir, output_dir,
+                                       "sysctl", False)
+
+    snap.add_fun_group("kernel.sched_latency_ns")
+
+    assert len(snap.fun_groups) == 1
+    assert "kernel.sched_latency_ns" in snap.fun_groups
+
+
+def test_add_fun_none_group():
+    """Create a snapshot and try to add functions into a None group."""
+    kernel_dir = "kernel/linux-3.10.0-957.el7"
+    output_dir = "snapshots/linux-3.10.0-957.el7"
+    snap = Snapshot.create_from_source(kernel_dir, output_dir,
+                                       None, False)
+
+    mod = LlvmKernelModule("net/core/skbuff.ll")
+    snap.add_fun("___pskb_trim", mod)
+
+    assert "___pskb_trim" in snap.fun_groups[None].functions
+    fun_desc = snap.fun_groups[None].functions["___pskb_trim"]
+    assert fun_desc.mod is mod
+    assert fun_desc.glob_var is None
+    assert fun_desc.tag is None
+
+
+def test_add_fun_sysctl_group():
+    """Create a snapshot and try to add functions into sysctl groups."""
+    kernel_dir = "kernel/linux-3.10.0-957.el7"
+    output_dir = "snapshots/linux-3.10.0-957.el7"
+    snap = Snapshot.create_from_source(kernel_dir, output_dir,
+                                       "sysctl", False)
+
+    snap.add_fun_group("kernel.sched_latency_ns")
+    mod = LlvmKernelModule("kernel/sched/debug.ll")
+    snap.add_fun("sched_debug_header",
+                 mod,
+                 "sysctl_sched_latency",
+                 "using_data_variable \"sysctl_sched_latency\"",
+                 "kernel.sched_latency_ns")
+
+    assert "sched_debug_header" in snap.fun_groups[
+        "kernel.sched_latency_ns"].functions
+    fun_desc = snap.fun_groups["kernel.sched_latency_ns"].functions[
+        "sched_debug_header"]
+    assert fun_desc.mod is mod
+    assert fun_desc.glob_var == "sysctl_sched_latency"
+    assert fun_desc.tag == "using_data_variable \"sysctl_sched_latency\""
+
+
+def test_get_modules():
+    """
+    Test getting all modules in the snapshot function lists.
+    Check if the snapshot returns a list of all modules of all groups in case
+    that multiple groups are present.
+    """
+    kernel_dir = "kernel/linux-3.10.0-957.el7"
+    output_dir = "snapshots/linux-3.10.0-957.el7"
+    snap = Snapshot.create_from_source(kernel_dir, output_dir,
+                                       "sysctl", False)
+
+    snap.add_fun_group("kernel.sched_latency_ns")
+    snap.add_fun_group("kernel.timer_migration")
+    snap.add_fun("sched_proc_update_handler",
+                 LlvmKernelModule("kernel/sched/fair.ll"), None,
+                 "proc_handler", "kernel.sched_latency_ns")
+    snap.add_fun("proc_dointvec_minmax", LlvmKernelModule("kernel/sysctl.ll"),
+                 None,
+                 "proc_handler", "kernel.timer_migration")
+
+    modules = snap.modules()
+    assert len(modules) == 2
+    assert set([m.llvm for m in modules]) == {"kernel/sched/fair.ll",
+                                              "kernel/sysctl.ll"}
+
+
+def test_get_by_name_functions():
+    """Get the module of inserted function by its name."""
+    kernel_dir = "kernel/linux-3.10.0-957.el7"
+    output_dir = "snapshots/linux-3.10.0-957.el7"
+    snap = Snapshot.create_from_source(kernel_dir, output_dir,
+                                       None, False)
+
+    mod_buff = LlvmKernelModule("net/core/skbuff.ll")
+    mod_alloc = LlvmKernelModule("mm/page_alloc.ll")
+    snap.add_fun("___pskb_trim", mod_buff)
+    snap.add_fun("__alloc_pages_nodemask", mod_alloc)
+
+    fun = snap.get_by_name("___pskb_trim")
+    assert fun.mod is mod_buff
+    fun = snap.get_by_name("__alloc_pages_nodemask")
+    assert fun.mod is mod_alloc
+
+
+def test_get_by_name_sysctls():
+    """Get the module of inserted function by its name and sysctl group."""
+    kernel_dir = "kernel/linux-3.10.0-957.el7"
+    output_dir = "snapshots/linux-3.10.0-957.el7"
+    snap = Snapshot.create_from_source(kernel_dir, output_dir,
+                                       "sysctl", False)
+
+    snap.add_fun_group("kernel.sched_latency_ns")
+    snap.add_fun_group("kernel.timer_migration")
+    mod_fair = LlvmKernelModule(
+        "snapshots-sysctl/linux-3.10.0-957.el7/kernel/sched/fair.ll")
+    mod_sysctl = LlvmKernelModule(
+        "snapshots-sysctl/linux-3.10.0-957.el7/kernel/sysctl.ll")
+    snap.add_fun("sched_proc_update_handler", mod_fair, None, "proc handler",
+                 "kernel.sched_latency_ns")
+    snap.add_fun("proc_dointvec_minmax", mod_sysctl, None, "proc handler",
+                 "kernel.timer_migration")
+
+    # Test that the function
+    fun = snap.get_by_name("proc_dointvec_minmax", "kernel.sched_latency_ns")
+    assert fun is None
+    fun = snap.get_by_name("proc_dointvec_minmax", "kernel.timer_migration")
+    assert fun.mod is mod_sysctl
+
+
+def test_filter():
+    """Filter snapshot functions."""
+    kernel_dir = "kernel/linux-3.10.0-957.el7"
+    output_dir = "snapshots/linux-3.10.0-957.el7"
+    snap = Snapshot.create_from_source(kernel_dir, output_dir,
+                                       None, False)
+
+    snap.add_fun("___pskb_trim", LlvmKernelModule("net/core/skbuff.ll"))
+    snap.add_fun("__alloc_pages_nodemask",
+                 LlvmKernelModule("mm/page_alloc.ll"))
+
+    snap.filter(["__alloc_pages_nodemask"])
+    assert len(snap.fun_groups[None].functions) == 1
+    assert "___pskb_trim" not in snap.fun_groups[None].functions
+    assert "__alloc_pages_nodemask" in snap.fun_groups[None].functions
+
+
+def test_to_yaml_functions():
+    """
+    Dump a snapshot with a single "None" group into YAML.
+    YAML string should contain the version of Diffkemp, source kernel
+    directory, a simple list of functions, each one having the "name",
+    "llvm", "glob_var" and "tag" fields set, and the kind of this list,
+    which should be a function list.
+    The LLVM paths in the YAML should be relative to the snapshot directory.
+    """
+    kernel_dir = "kernel/linux-3.10.0-957.el7"
+    output_dir = "snapshots/linux-3.10.0-957.el7"
+    snap = Snapshot.create_from_source(kernel_dir, output_dir,
+                                       None, False)
+
+    snap.add_fun("___pskb_trim", LlvmKernelModule(
+        "snapshots/linux-3.10.0-957.el7/net/core/skbuff.ll"))
+    snap.add_fun("__alloc_pages_nodemask", LlvmKernelModule(
+        "snapshots/linux-3.10.0-957.el7/mm/page_alloc.ll"))
+
+    yaml_str = snap.to_yaml()
+    yaml_snap = yaml.safe_load(yaml_str)
+
+    assert len(yaml_snap) == 1
+    yaml_dict = yaml_snap[0]
+    assert len(yaml_dict) == 4
+    assert len(yaml_dict["list"]) == 2
+    assert set([f["name"] for f in yaml_dict["list"]]) ==\
+        {"___pskb_trim",
+         "__alloc_pages_nodemask"}
+
+    for f in yaml_dict["list"]:
+        if f["name"] == "___pskb_trim":
+            assert f["llvm"] == "net/core/skbuff.ll"
+        elif f["name"] == "__alloc_pages_nodemask":
+            assert f["llvm"] == "mm/page_alloc.ll"
+
+
+def test_to_yaml_sysctls():
+    """
+    Dump a snapshot with multiple sysctl groups into YAML.
+    YAML string should contain the version of Diffkemp, source kernel
+    directory, a simple list of function groups, each one containing a
+    function list with the "name", "llvm", "glob_var" and "tag" fields set,
+    and the kind of this list, which should be a group list.
+    The LLVM paths in the YAML should be relative to the snapshot directory.
+    """
+    kernel_dir = "kernel/linux-3.10.0-957.el7"
+    output_dir = "snapshots-sysctl/linux-3.10.0-957.el7"
+    snap = Snapshot.create_from_source(kernel_dir, output_dir,
+                                       "sysctl", False)
+
+    snap.add_fun_group("kernel.sched_latency_ns")
+    snap.add_fun_group("kernel.timer_migration")
+    snap.add_fun("sched_proc_update_handler",
+                 LlvmKernelModule(
+                     "snapshots-sysctl/linux-3.10.0-957.el7/"
+                     "kernel/sched/fair.ll"),
+                 None, "proc handler", "kernel.sched_latency_ns")
+    snap.add_fun("proc_dointvec_minmax",
+                 LlvmKernelModule(
+                     "snapshots-sysctl/linux-3.10.0-957.el7/kernel/sysctl.ll"),
+                 None, "proc handler", "kernel.timer_migration")
+
+    yaml_str = snap.to_yaml()
+    yaml_snap = yaml.safe_load(yaml_str)
+    assert len(yaml_snap) == 1
+    yaml_dict = yaml_snap[0]
+    assert len(yaml_dict) == 4
+    assert len(yaml_dict["list"]) == 2
+    assert set([g["sysctl"] for g in yaml_dict["list"]]) == {
+        "kernel.sched_latency_ns",
+        "kernel.timer_migration"}
+
+    for g in yaml_dict["list"]:
+        assert len(g["functions"]) == 1
+        if g["sysctl"] == "kernel.sched_latency_ns":
+            assert g["functions"][0] == {
+                "name": "sched_proc_update_handler",
+                "llvm": "kernel/sched/fair.ll",
+                "glob_var": None,
+                "tag": "proc handler"
+            }
+        elif g["sysctl"] == "kernel.timer_migration":
+            assert g["functions"][0] == {
+                "name": "proc_dointvec_minmax",
+                "llvm": "kernel/sysctl.ll",
+                "glob_var": None,
+                "tag": "proc handler"
+            }
