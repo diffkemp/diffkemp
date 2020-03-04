2efe8d0a440029f2cc768363b7c7d25874a9c666
psilling <petr.silling@centrum.cz>
2020-03-04 12:09:32 +0100

    Docker: Add podman support to the starting script

--- docker/diffkemp-devel/run-container.sh
+++ docker/diffkemp-devel/run-container.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+COMMAND="docker"
+
+# Use podman if docker is not available.
+if [ ! -x "$(command -v docker)" ]; then
+    COMMAND="podman"
+fi
+
 # Prerequisity is an existing docker image called 'diffkemp-devel' built from
 # the provided Dockerfile
-docker run \
-	-ti --security-opt seccomp=unconfined \
+"$COMMAND" run \
+    -ti --security-opt seccomp=unconfined \
     -m 8g \
     --cpus 3 \
-	-v $PWD:/diffkemp:Z \
-	-w /diffkemp \
-	viktormalik/diffkemp-devel
+    -v $PWD:/diffkemp:Z \
+    -w /diffkemp \
+    viktormalik/diffkemp-devel
+
