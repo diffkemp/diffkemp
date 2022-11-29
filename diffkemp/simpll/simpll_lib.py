6308f657abd1d742a416368d1c0bdb2407bb497a
Viktor Malik <viktor.malik@gmail.com>
2022-11-29 12:30:12 +0100

    simpll-lib: improve Python imports
    There are several ways we want to support import of the SimpLL library:
    - from a local build directory (with supporting mutliple build dirs)
    - from a local installation
    - from a normal system path installation
    
    To achieve this, this commit introduces several changes:
    - _simpll.abi3.so is copied (instead of moving) from its default
      location into the build directory,
    - the copy is not done if the file is not in its default location
      (happens e.g. in the RPM build),
    - _simpll imports are done dynamically when the local build dir is tried
      first and only if it fails, it is imported from the default location
      (which covers both the local and the system installation).

--- diffkemp/simpll/simpll_lib.py
+++ diffkemp/simpll/simpll_lib.py
@@ -2,9 +2,14 @@ import sys
 from diffkemp.utils import get_simpll_build_dir
 
 # Dynamically import the SimpLL C extension module.
-# This way, we can prettily import the module objects into other files.
-sys.path.append(get_simpll_build_dir())
-import _simpll  # noqa E402
+# First try to import from the current build directory (to allow multiple
+# parallel local builds).
+# If that fails, import from the default location.
+try:
+    sys.path.append(get_simpll_build_dir())
+    _simpll = __import__("_simpll")
+except ImportError:
+    _simpll = __import__("diffkemp.simpll._simpll", fromlist=[None])
 
 lib = _simpll.lib
 ffi = _simpll.ffi
