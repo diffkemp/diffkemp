f8b1f65d94c4eda0bd832baa9b8bff95c6e73552
Lukas Petr <lukas.petr.dev@gmail.com>
2023-03-18 14:00:19 +0100

    Viewer: Add tests for components

---
+++ view/src/tests/FunctionListing.test.jsx
@@ -0,0 +1,52 @@
+// Unit tests of FunctionListing component
+// Author: Lukas Petr
+
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
+import FunctionListing from '../components/FunctionListing';
+
+test('headline should be visible', () => {
+  render(
+    <FunctionListing
+      headline="Function"
+      functions={[]}
+      onFunctionSelect={() => {
+
+      }}
+    />,
+  );
+  expect(screen.getByText('Function')).toBeVisible();
+});
+
+test('function names should be visible', () => {
+  const names = ['free', 'malloc', 'puts', 'printf'];
+  render(
+    <FunctionListing
+      headline="Function"
+      functions={names}
+      onFunctionSelect={() => {
+
+      }}
+    />,
+  );
+  expect(screen.getByText('malloc')).toBeVisible();
+  expect(screen.getByText('free')).toBeVisible();
+  expect(screen.getByText('puts')).toBeVisible();
+  expect(screen.getByText('printf')).toBeVisible();
+});
+
+test('click on function should return its name', () => {
+  const names = ['free', 'malloc', 'printf'];
+  const handleSelect = jest.fn();
+  render(
+    <FunctionListing
+      headline="Function"
+      functions={names}
+      onFunctionSelect={handleSelect}
+    />,
+  );
+  userEvent.click(screen.getByText('malloc'));
+  expect(handleSelect).toHaveBeenCalledWith('malloc');
+});
