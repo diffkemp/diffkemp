73860f69f241ae445aac088f0aca9fb3ce727c33
Lukas Petr <lukas.petr.dev@gmail.com>
2023-06-28 21:11:05 +0200

    Viewer: Moved navigation handling to component
    Setting of compared and differing functions is moved to navigation
    component to give navigation better control on what is shown.

--- view/src/components/ResultNavigation.jsx
+++ view/src/components/ResultNavigation.jsx
@@ -1,5 +1,7 @@
 // Visualisation of location in results for user navigation
 // Author: Lukas Petr
+
+import { useCallback } from 'react';
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import Container from 'react-bootstrap/Container';
 
@@ -7,24 +9,37 @@ import Container from 'react-bootstrap/Container';
  * @param {Object} props
  * @param {string} props.comparedFunction Name of a compared function.
  * @param {string} props.diffFunction Name of a differing function.
- * @param {Function} props.onResultsClick Function which is called
- * when results is clicked.
- * @param {Function} props.onCompareClick Function which is called
- * when compared function is clicked.
+ * @param {Function} props.setComparedFunction Callback function for seting new compared function.
+ * @param {Function} props.setDiffFunction Callback function for seting new differing function.
  * @returns
  */
 export default function ResultNavigation({
   comparedFunction,
   diffFunction,
-  onResultsClick,
-  onCompareClick,
-  onDifferingClick,
+  setComparedFunction,
+  setDiffFunction,
 }) {
+  const handleResultsClick = useCallback(() => {
+    setComparedFunction(null);
+    setDiffFunction(null);
+  }, [setComparedFunction, setDiffFunction]);
+
+  const handleCompareClick = useCallback(() => {
+    setDiffFunction(null);
+  }, [setDiffFunction]);
+
+  const handleDifferingClick = useCallback(() => {
+    setComparedFunction(null);
+  }, [setComparedFunction]);
+
   let compItem = null;
   // location in compared function
   if (comparedFunction || diffFunction) {
     compItem = (
-      <Breadcrumb.Item onClick={onCompareClick} title="Compared function">
+      <Breadcrumb.Item
+        onClick={handleCompareClick}
+        title="Compared function"
+      >
         {comparedFunction || '*'}
       </Breadcrumb.Item>
     );
@@ -33,13 +48,13 @@ export default function ResultNavigation({
   return (
     <Container>
       <Breadcrumb className="result-nav d-inline-block">
-        <Breadcrumb.Item onClick={onResultsClick}>
+        <Breadcrumb.Item onClick={handleResultsClick}>
           Results
         </Breadcrumb.Item>
         {compItem}
         {diffFunction && (
         <Breadcrumb.Item
-          onClick={onDifferingClick}
+          onClick={handleDifferingClick}
           title="Differing function"
         >
           {diffFunction}
