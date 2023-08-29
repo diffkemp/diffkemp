3a22e3fde224aec1359acae6c76d448584d77ae2
Lukas Petr <lukas.petr.dev@gmail.com>
2023-08-29 15:33:20 +0200

    Viewer: Extract style imports to file
    Extracting style imports to file so the same imports
    can be also used for tests.

---
+++ view/src/setup.jsx
@@ -0,0 +1,8 @@
+// Setup used in application
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-diff-view/style/index.css';
+import 'prismjs/themes/prism.min.css';
+import 'prism-color-variables/variables.css';
+import 'prism-color-variables/themes/visual-studio.css';
+import './style.css';
