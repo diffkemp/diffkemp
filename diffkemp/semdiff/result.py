8d69cc86c415ece62743ff1b03355cf7ecc7d15a
Lukas Petr <lukas.petr.dev@gmail.com>
2023-12-19 10:58:30 +0100

    Result Callstack refactoring
    Extract splitting symbol name from a symbol kind to a separate
    function so it can be reused.
    Also invert a condition to decrease indentation.

--- diffkemp/semdiff/result.py
+++ diffkemp/semdiff/result.py
@@ -81,6 +81,15 @@ class Result:
                      "line": call["line"]}
                     for call in self.calls]
 
+        @staticmethod
+        def split_name_and_kind(call):
+            """Returns tuple (name, kind) for a call."""
+            split = call["name"].split()
+            if len(split) == 1:
+                return (split[0], "function")
+            # The expected form is "name (kind)"
+            return (split[0], split[1][1:-1])
+
         def get_symbol_names(self, compared_fun):
             """
             Returns a tuple containing three sets of symbol names which appear
@@ -89,30 +98,24 @@ class Result:
             - macro names (set of strings)
             - type names (set of string pairs)
                 (type name, name of function in which the type is used)
-
             :param compared_fun: Name of compared function.
             """
             function_names = set()
             macro_names = set()
             type_names = set()
-            if self.calls:
-                for index, call in enumerate(self.calls):
-                    # getting symbol name
-                    name = call["name"]
-                    kind = "function"
-                    if " " in name:
-                        name, kind = name.split(" ")
-                    if kind == "function":
-                        function_names.add(name)
-                    elif kind == "(macro)":
-                        macro_names.add(name)
-                    elif kind == "(type)":
-                        # name of type and name of function
-                        # in which it is used
-                        parent_name = (self.calls[index - 1]["name"]
-                                       if index >= 1
-                                       else compared_fun)
-                        type_names.add((name, parent_name))
+            if not self.calls:
+                return function_names, macro_names, type_names
+            for index, call in enumerate(self.calls):
+                name, kind = self.split_name_and_kind(call)
+                if kind == "function":
+                    function_names.add(name)
+                elif kind == "macro":
+                    macro_names.add(name)
+                elif kind == "type":
+                    # name of type and name of function in which it is used
+                    parent_name = (self.calls[index - 1]["name"] if index >= 1
+                                   else compared_fun)
+                    type_names.add((name, parent_name))
             return function_names, macro_names, type_names
 
         def __str__(self):
