da968f74a4e4668533b8b3c05910b8fbc5cdbcee
Lukas Petr <lukas.petr.dev@gmail.com>
2024-04-06 13:29:55 +0200

    build tests: testing compilation with linking

--- tests/unit_tests/build_test.py
+++ tests/unit_tests/build_test.py
@@ -80,3 +80,17 @@ def test_make_based_with_assembly(tmp_path):
     db_file_content = get_db_file_content(output_dir)
     assert "mod.ll" not in db_file_content
     assert "sub.ll" not in db_file_content
+
+
+def test_make_based_with_linking(tmp_path):
+    """Tests make based project which contains linking of file(s)."""
+    output_dir = str(tmp_path)
+    args = Arguments(MAKE_BASED_PROJECT_DIR, output_dir,
+                     target=["with-linking"])
+    build(args)
+    # When linking *.o files, appropriate .ll files
+    # should be linked with llvm-link and saved as `.llw`.
+    # Note: The .llw is not copied to snapshot dir.
+    assert "file.so.llw" in os.listdir(MAKE_BASED_PROJECT_DIR)
+    db_file_content = get_db_file_content(output_dir)
+    assert "file.so.llw" in db_file_content
