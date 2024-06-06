3d295f8eb7a10e507d09c69c1547f7db578ee1ac
Daniel Kříž <Daniel.kriz@protonmail.com>
2024-06-06 11:20:49 +0200

    pyproject: move to pyproject from setuptools
    Python community is moving towards storing metadata for projects inside
    TOML files. With this commit we are also moving into this direction but
    unfortunately due to cffi we cannot move to `pyproject.toml` as the cffi
    developers do not plan to support it currently.
    
    We also can get rid of `requirements.txt` as it is now obsolete, as we
    can define the dependencies inside of `pyproject.toml` and they are
    installed automatically with `pip install .` (there is no need to use
    `-r` option separately).
    
    It is also possible to generate diffkemp executable automatically with
    pip.

--- setup.py
+++ setup.py
@@ -1,19 +1,6 @@
-from setuptools import setup, find_packages
-from diffkemp.utils import get_simpll_build_dir
-import os
-import subprocess
-
-setup(name="diffkemp",
-      version="0.6.0",
-      description="A tool for semantic difference analysis of kernel functions",
-      author="Viktor Malik",
-      author_email="vmalik@redhat.com",
-      url="https://github.com/viktormalik/diffkemp",
-      packages=find_packages(),
-      setup_requires=["cffi"],
-      install_requires=["pyyaml", "cffi"],
-      cffi_modules=["./diffkemp/simpll/simpll_build.py:ffibuilder"])
-
-if os.path.isfile("diffkemp/simpll/_simpll.abi3.so"):
-    build_dir = get_simpll_build_dir()
-    subprocess.run(["cp", "diffkemp/simpll/_simpll.abi3.so", build_dir])
+from setuptools import setup
+setup(
+    setup_requires=["cffi>=1.0.0"],
+    cffi_modules=["./diffkemp/simpll/simpll_build.py:ffibuilder"],
+    install_requires=["cffi>=1.0.0"],
+)
