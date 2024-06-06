7799f5ab429790ccbaac272b4a5022f58503c8d2
Daniel Kříž <Daniel.kriz@protonmail.com>
2024-06-06 10:04:29 +0200

    CMake: always rebuild simpll CFFI and move build to build dir
    Moving the build of CFFI bindings to the build dir allows us to do
    parallel builds. And if we are rebuilding the CFFI every time when
    important files have changed (i.e. simpll_build scipt), then we are
    leveraging the CFFI build to cmake and we do not have to call pip every
    time.
    
    We also have to reflect this change in the python build script. As we
    now have to support two possible locations of it. First is the original
    one, that `setuptools` rely on. And the second is in the build
    directory.
    
    We can recognize whether the build script is ran manually or by the
    `setuptools` that to the fact, that the `setuptools` build calls this
    script with additional arguments for the release purposes.
    
    Because of this commit, the generated .so library is located in (1)
    build directory under `diffkemp/simpll` or (2) in site-packages in
    `diffkemp/diffkemp/simpll`.
    
    simpll_build: use implicit import
    
    Due to some internals of pip this code would fail, if the import is not implicit.
    It probably requires some pip metadata, and we need to be able to use this without any.
    
    SimpLL build: Add comment -- determining root dir
    
    Update documentation of `simpll_lib.py`

--- diffkemp/simpll/simpll_build.py
+++ diffkemp/simpll/simpll_build.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 from cffi import FFI
 from subprocess import check_output
-from diffkemp.utils import get_simpll_build_dir
+import os
+import sys
 
 
 def get_c_declarations(header_filename):
@@ -20,9 +21,23 @@ def get_c_declarations(header_filename):
         return "".join(lines[start:end])
 
 
-ffibuilder = FFI()
+def get_root_dir(path, is_develop_build):
+    # The build script is located in CMAKE_BINARY_DIR/diffkemp/simpll, hence
+    # we have to move out of it
+    if is_develop_build:
+        return os.path.abspath(f"{path}/../../..")
+    return os.path.abspath(f"{path}/../..")
 
-ffibuilder.cdef(get_c_declarations("diffkemp/simpll/library/FFI.h"))
+
+ffibuilder = FFI()
+location = os.path.dirname(os.path.abspath(__file__))
+# We can get the project root directory by recognizing whether the script is
+# ran manually or by the `setuptools`. The `setuptools` build calls this script
+# with additional arguments (_in_process.py bdist_wheel --dist-info-dir ...).
+root_dir = get_root_dir(location, is_develop_build=(len(sys.argv) == 1))
+path_to_ffi_header = "diffkemp/simpll/library/FFI.h"
+
+ffibuilder.cdef(get_c_declarations(f"{root_dir}/{path_to_ffi_header}"))
 
 llvm_components = ["irreader", "passes", "support"]
 llvm_cflags = check_output(["llvm-config", "--cflags"])
@@ -37,15 +52,19 @@ llvm_ldflags = list(filter(lambda x: x != "",
 llvm_libs = list(filter(lambda x: x != "",
                         llvm_libs.decode("ascii").strip().split()))
 
-simpll_link_arg = "-L{}/diffkemp/simpll".format(get_simpll_build_dir())
+env_var = "SIMPLL_BUILD_DIR"
+build_dir = os.environ[env_var] if env_var in os.environ else "build"
+
+simpll_link_arg = f"-L{build_dir}/diffkemp/simpll"
+
+include_path = f"{root_dir}/diffkemp/simpll"
 
 ffibuilder.set_source(
-    "diffkemp.simpll._simpll", '#include <library/FFI.h>',
+    "_simpll", "#include <library/FFI.h>",
     language="c++",
-    libraries=['simpll-lib'],
-    extra_compile_args=["-Idiffkemp/simpll"] + llvm_cflags,
-    extra_link_args=[simpll_link_arg, "-lstdc++"] + llvm_ldflags +
-    llvm_libs + ["-lz3"])
+    libraries=["simpll-lib"],
+    extra_compile_args=[f"-I{include_path}"] + llvm_cflags,
+    extra_link_args=[simpll_link_arg, "-lstdc++"] + llvm_ldflags + llvm_libs)
 
 if __name__ == "__main__":
     ffibuilder.compile()
