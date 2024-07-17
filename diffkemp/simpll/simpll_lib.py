a651489e5f6d9812663d00695c670382352aec16
Daniel Kříž <Daniel.kriz@protonmail.com>
2024-07-17 18:45:34 +0200

    Simplify SimpLL importing
    Because we are setting the PYTHONPATH for each binary or we are using
    virtualenv's packages, then we do not have to use the whole path.

--- diffkemp/simpll/simpll_lib.py
+++ diffkemp/simpll/simpll_lib.py
@@ -2,10 +2,6 @@
 # First try to import from the current build directory (to allow multiple
 # parallel local builds).
 # If that fails, import from the default location.
-try:
-    _simpll = __import__('_simpll')
-except ImportError:
-    _simpll = __import__('diffkemp.simpll._simpll')
-
+_simpll = __import__("_simpll")
 lib = _simpll.lib
 ffi = _simpll.ffi
