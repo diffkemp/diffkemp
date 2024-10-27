bb0f87c22353aed1f0715790012ec6e1eff7dfac
František Nečas <frantisek.necas@protonmail.com>
2024-10-27 16:59:54 +0100

    Find differing code snippets
    Before encoding the equivalence of differing blocks into an SMT formula,
    we need to find the code blocks to compare. We do so similarly to how
    relocations are detected, i.e., we search for a synchronization point
    after which the remainder of the basic blocks are semantically
    equivalent.

--- diffkemp/simpll/SmtBlockComparator.h
+++ diffkemp/simpll/SmtBlockComparator.h
@@ -21,6 +21,18 @@
 
 using namespace llvm;
 
+class SMTException : public std::exception {};
+
+/// An exception thrown when no analyzable code snippets are found, i.e.
+/// there are no snippets after which the remaining basic block instructions
+/// can be synchronized.
+class NoSynchronizationPointException : public SMTException {
+  public:
+    virtual const char *what() const noexcept {
+        return "No synchronization point found";
+    }
+};
+
 /// A class for comparing short sequential code snippets using an SMT solver.
 class SmtBlockComparator {
   public:
@@ -35,8 +47,19 @@ class SmtBlockComparator {
                 BasicBlock::const_iterator &InstR);
 
   private:
+    DenseMap<const Value *, int> sn_mapL_backup;
+    DenseMap<const Value *, int> sn_mapR_backup;
+    std::unordered_map<int, std::pair<const Value *, const Value *>>
+            mappedValuesBySnBackup;
+
     const Config &config;
     const DifferentialFunctionComparator *fComp;
+
+    /// Moves InstL and InstR iterators to the end of the snippet to compare.
+    /// The snippet must be followed by a synchronized instruction. If no such
+    /// instruction is found, the whole basic block is to be considered.
+    void findSnippetEnd(BasicBlock::const_iterator &InstL,
+                        BasicBlock::const_iterator &InstR);
 };
 
 #endif // DIFFKEMP_SMTBLOCKCOMPARATOR_H
